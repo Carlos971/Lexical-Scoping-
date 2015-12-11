d714b320b1b7b1884b0fa95b3ab544b3a5e55e9a
CARLETaimy <a.carlet.14@groupe-esigelec.fr>
2015-12-11 11:47:35 +0100

    DEPOT AVEC AFFICHER, POSTULER, ET CREER UNE EQUIPE + BASE DONNEES

---
+++ depot 11.12.15/PageChefEquipe.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!-- APPEL DE LA PAGE OU IL Y A LA FONCTION -->
+	<?php
+		require_once('param.inc.php');
+	?>
+<html>
+ <head>
+  <meta charset="utf-8">
+ 	<title>----Page chef équipe----</title>
+	<link rel="stylesheet" href="stylePageAccueil.css">
+ </head>
+ <body>
+ <!-- BANNIERE STRUCTURE -->
+  <header> </header>
+
+ <!-- BANDE BLEUE -->
+	<p class ="p1">
+		<img src="kaggle.jpg" alt="logo"> 
+		<span class="marge">
+		<!-- SESSION-->
+			<?php
+			session_start();
+			if($_SESSION['pseudo']){
+				echo"Bonjour ".$_SESSION['pseudo']." ! <br/><a href='Deconnexion.php'> Se déconnecter</a>";
+			}else header('Location:PageAcceuil.php');
+			?>
+		</span>
+		<br>
+</p>
+
+<p class ="p4">*<a href="PageListePostulant.php">AFFICHER LA LISTE DES POSTULANTS</a><br><br>
+	*<a href="PageListeReunion.php">AFFICHER LA LISTE DES REUNIONS </a> </p><br>
+	
+		
+	<br><br>
+	<!-- "Tous droits réservés" centré et en gras -->
+<body>
+</html>
